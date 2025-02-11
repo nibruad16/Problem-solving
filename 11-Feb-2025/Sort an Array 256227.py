5dedd8ecde5edd1ad6046943c7c5cdd41115bf4b
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-11 14:08:24 +0300

    Created 'Sort an Array' by A2SV Hub

---
+++ 11-Feb-2025/Sort an Array 256227.py
@@ -0,0 +1,7 @@
+# Problem: Sort an Array - https://leetcode.com/problems/sort-an-array/description/
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        new = sorted(nums)
+        return new
+
