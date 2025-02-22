71a5c790222b4d53020b48a27b92e2b16f39043c
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-22 09:57:55 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 22-Feb-2025/Alternating Subsequence 280129.py
@@ -0,0 +1,26 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    mx = -1e11
+    sum = 0
+    check = True
+    for i in range(n):
+        if check:
+            if a[i] < 0:
+                if mx > -1e10:
+                    sum += mx
+                mx = -1e11
+                check = False
+        else:
+            if a[i] > 0:
+                if mx > -1e10:
+                    sum += mx
+                mx = -1e11
+                check = True
+        mx = max(a[i], mx)
+    if mx > -1e10:
+        sum += mx
+    print(sum)
