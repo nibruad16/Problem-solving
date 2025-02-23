a7f1696753a16109ed57f328c52085372c4f5f0d
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-23 15:18:23 +0300

    Created 'Maximum Sum Obtained of Any Permutation' by A2SV Hub

---
+++ 23-Feb-2025/Maximum Sum Obtained of Any Permutation 281063.py
@@ -0,0 +1,23 @@
+# Problem: Maximum Sum Obtained of Any Permutation - https://leetcode.com/problems/maximum-sum-obtained-of-any-permutation/description/
+
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+
+        n = len(nums)
+        freq = [0] * n
+        for start, end in requests:
+            freq[start] += 1
+            if end + 1 < n:
+                freq[end + 1] -= 1
+        
+        for i in range(1, n):
+            freq[i] += freq[i - 1]
+        
+        freq.sort(reverse=True)
+        nums.sort(reverse=True)
+        
+        total_sum = 0
+        for i in range(n):
+            total_sum = (total_sum + freq[i] * nums[i]) % (10**9 + 7)
+        
+        return total_sum
