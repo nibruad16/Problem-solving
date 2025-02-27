0bdd48c4f883703e0a4567e37066ce9f0750a08b
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-27 16:03:16 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 27-Feb-2025/D - Repeating Cipher 285979.py
@@ -0,0 +1,14 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+x = int(input())
+s = input()
+
+l = 0
+i = 1
+ans = ""
+while l < x:
+    ans += s[l]
+    l += i
+    i += 1
+print(ans)
+
