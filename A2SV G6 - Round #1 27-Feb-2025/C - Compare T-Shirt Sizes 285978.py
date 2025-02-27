bd4d29a6dbf8dc4454a4935be5441b705ea4e24f
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-27 16:03:08 +0300

    Created 'C - Compare T-Shirt Sizes' by A2SV Hub

---
+++ A2SV G6 - Round #1 27-Feb-2025/C - Compare T-Shirt Sizes 285978.py
@@ -0,0 +1,42 @@
+# Problem: C - Compare T-Shirt Sizes - https://codeforces.com/gym/585107/problem/C
+
+x = int(input())
+
+for i in range(x):
+    s1 ,s2 = map(str , input().split())
+    if s1[-1] == "M" and s2[-1] == "S":
+        print(">")
+    elif s1[-1] == "S" and s2[-1] == "M":
+        print("<")
+    elif s1[-1] == "L" and s2[-1] == "M":
+        print(">")
+    elif  s1[-1] == "M" and s2[-1] == "L":
+        print("<")
+    elif s1[-1] == "M" and s2[-1] == "M":
+        print("=")
+    elif s1[-1] == "S" and s2[-1] == "L":
+        print("<")
+    elif s1[-1] == "L" and s2[-1] == "S":
+        print(">")
+    elif s1[-1] == "S" and s2[-1] == "S":
+        s1c = s1.count("X")
+        s2c = s2.count("X")
+        
+        if s1c > s2c:
+            print("<")
+        elif s1c < s2c:
+            print(">")
+        else:
+            print("=")
+    else:
+        s1[-1] == "L" and s2[-1] == "L"
+        s1c = s1.count("X")
+        s2c = s2.count("X")
+        
+        if s1c > s2c:
+            print(">")
+        elif s1c < s2c:
+            print("<")
+        else:
+            print("=")
+    
