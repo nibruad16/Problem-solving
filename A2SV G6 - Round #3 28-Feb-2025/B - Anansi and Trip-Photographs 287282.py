5004c230316861b222abf1c4614017493439e98f
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-28 22:04:51 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 28-Feb-2025/B - Anansi and Trip-Photographs 287282.py
@@ -0,0 +1,18 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+new = int(input())
+for _ in range(new):
+    a,b = map(int,input().split())
+    arr = [int(i) for i in input().split()]
+    arr.sort()
+    first = arr[:a]
+    sec = arr[a:]
+
+    Flag = True
+    for i in range(len(first)):
+        if sec[i] - first[i] < b:
+            Flag = False
+    if Flag:
+        print("YES")
+    else:
+        print("NO")
