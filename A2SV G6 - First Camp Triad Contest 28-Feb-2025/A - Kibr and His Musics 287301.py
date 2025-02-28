185b16647d49209d3f0c1fc0d11ab3a61d982bde
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-28 22:26:34 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 28-Feb-2025/A - Kibr and His Musics 287301.py
@@ -0,0 +1,22 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+n , m = [int(i) for i in input().split()]
+array = []
+for i in range(n):
+    c, t= [int(i) for i in input().split()]
+    array.append(c*t)
+prefix_sum = [0]
+for i in range(n):
+    prefix_sum.append(prefix_sum[-1]+array[i])
+moment = [int(i) for i in input().split()]
+ 
+ 
+j = 1
+i = 0
+while i < m:
+    if moment[i] <= prefix_sum[j]:
+        print(j)
+        i+=1
+    else:
+        j+=1
+ 
