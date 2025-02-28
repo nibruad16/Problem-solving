496272eea1d483df836f6efc464dc797c3a24e33
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-28 22:26:44 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 28-Feb-2025/B - Kidus Couldn't Sleep 287302.py
@@ -0,0 +1,16 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+n , m = [int(i) for i in input().split()]
+
+nums=  [int(i) for i in input().split()]
+
+
+prefix_sum = [0]
+for i in range(n):
+    prefix_sum.append(prefix_sum[-1] + nums[i])
+sum_ = 0
+j = 0
+for i in range(m, n+1):
+    sum_+=(prefix_sum[i]-prefix_sum[j])
+    j+=1
+print(sum_/(n-m+1))
