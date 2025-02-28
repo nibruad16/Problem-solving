aef8de60448cce30b0d16b85b5c79a35e00c71ed
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-02-28 22:04:45 +0300

    Created 'A - Nth Digit in Sequence' by A2SV Hub

---
+++ A2SV G6 - Round #3 28-Feb-2025/A - Nth Digit in Sequence 287281.py
@@ -0,0 +1,6 @@
+# Problem: A - Nth Digit in Sequence - https://codeforces.com/gym/588468/problem/A
+
+new = int(input())
+arr = [int(i) for i in range(1,1001)]
+me = "".join(str(i) for i in arr)
+print(me[new-1])
