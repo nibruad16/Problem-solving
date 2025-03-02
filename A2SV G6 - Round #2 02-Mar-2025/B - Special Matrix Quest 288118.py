6ee1c991495284babf24859b84bfc0a59d7e2d11
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-02 16:10:50 +0300

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 02-Mar-2025/B - Special Matrix Quest 288118.py
@@ -0,0 +1,17 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+x = int(input())
+matrix = []
+for _ in range(x):
+    nums = [int(i) for i in input().split()]
+    matrix.append(nums)
+ans = 0
+mid = x // 2
+for row in range(len(matrix)):
+    for col in range(len(matrix[0])):
+        if row == col or row + col == x - 1 or row == mid or col == mid:
+            ans += matrix[row][col]
+print(ans)
+
+
+
