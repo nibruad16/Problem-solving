a16c08ea5d25d133def3dfccae235673c2dc891c
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-02 16:20:13 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 02-Mar-2025/C - The Splitting Game 288129.py
@@ -0,0 +1,25 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+x = int(input())
+for _ in range(x):
+    y = int(input())
+    s = input()
+    set_ = set()
+    track = Counter(s)
+    ans = 0
+    for i in range(y -1):
+        char = s[i]
+
+        set_.add(s[i])
+        track[char] -= 1
+
+        if track[char] == 0:
+            del track[char]
+        
+        temp = len(set_) + len(track)
+        ans = max(ans , temp)
+    print(ans)
+
+
+
