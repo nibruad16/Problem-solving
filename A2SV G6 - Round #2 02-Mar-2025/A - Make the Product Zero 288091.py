a66d2b105785ece4458e0671c128710fedb4bcb3
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-02 15:02:23 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 02-Mar-2025/A - Make the Product Zero 288091.py
@@ -0,0 +1,8 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+x = int(input())
+nums = list(map(int,input().split()))
+ans = []
+for i in nums:
+        ans.append(abs(i))
+print(min(ans))
