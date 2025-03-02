2fb4c65e0b72da7388df5a8bfa290e7dadb785df
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-02 14:07:09 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 02-Mar-2025/A - Make the Product Zero 288066.py
@@ -0,0 +1,11 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+x = int(input())
+arr = [int(i) for i in input().split()]
+
+new = []
+
+for i in arr:
+    temp = abs(i)
+    new.append(temp)
+print(min(new))
