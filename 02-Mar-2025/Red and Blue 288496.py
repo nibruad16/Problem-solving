ea693b73a28726cb786fcdc5d591a6d88805428b
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-03 00:44:40 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 02-Mar-2025/Red and Blue 288496.py
@@ -0,0 +1,26 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+x = int(input())
+for _ in range(x):
+    n = int(input())
+    r = [int(i) for i  in input().split()]
+    m = int(input())
+    b = [int(i) for i  in input().split()]
+
+    presum = []
+    cum = []
+   
+
+    presum.append(r[0])
+    for i in range(1,len(r)):
+        presum.append(presum[i-1]+r[i])
+    presum.append(0)
+
+    cum.append(b[0])
+    for i in range(1,len(b)):
+        cum.append(cum[i-1]+b[i])
+    cum.append(0)
+
+    print(max(cum) + max(presum))
+        
+    
