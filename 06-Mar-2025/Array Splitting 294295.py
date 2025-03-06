553fc4eea07e7d6a05df0e168253cea83079902b
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-06 16:02:19 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 06-Mar-2025/Array Splitting 294295.py
@@ -0,0 +1,18 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+x,y = map(int,input().split())
+arr = [int(i) for i in input().split()]
+
+diff = []
+for i in range(1,len(arr)):
+    diff.append(arr[i] - arr[i-1])
+
+diff.sort(reverse=True)
+
+gap = arr[-1] - arr[0]
+
+for i in range(y-1):
+    gap -= diff[i]
+
+print(gap)
+
