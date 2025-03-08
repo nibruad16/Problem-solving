b1e152ce7bab787b7ff905133f14a5c9baf7df07
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-08 20:00:45 +0300

    Created 'D - Bomb Detection Validation' by A2SV Hub

---
+++ A2SV G6 - Round #2 08-Mar-2025/D - Bomb Detection Validation 298068.py
@@ -0,0 +1,44 @@
+# Problem: D - Bomb Detection Validation - https://codeforces.com/gym/586960/problem/D
+
+n, m = map(int, input().split())  
+grid = []
+
+for _ in range(n):
+    row = input()
+    grid.append(row)
+
+
+directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+def is_inbound(x, y):
+   
+    return 0 <= x < n and 0 <= y < m
+
+def count_bombs(x, y):
+   
+    bomb_count = 0
+    for dx, dy in directions:
+        ni, nj = x + dx, y + dy
+        if is_inbound(ni, nj) and grid[ni][nj] == '*':
+            bomb_count += 1
+            
+    return bomb_count
+
+valid = True  
+
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == '*':
+            continue 
+        
+        bomb_count = count_bombs(i, j)
+        
+        if grid[i][j] == '.': 
+            if bomb_count > 0:
+                valid = False
+        else: 
+            if int(grid[i][j]) != bomb_count:
+                valid = False
+
+
+print("YES" if valid else "NO")
