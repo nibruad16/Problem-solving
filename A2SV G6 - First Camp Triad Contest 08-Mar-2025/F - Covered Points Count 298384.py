46881c01051a137518f985f998cbba39d1d207ae
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 02:49:55 +0300

    Created 'F - Covered Points Count' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 08-Mar-2025/F - Covered Points Count 298384.py
@@ -0,0 +1,27 @@
+# Problem: F - Covered Points Count - https://codeforces.com/gym/589822/problem/F
+
+import sys
+from collections import defaultdict
+
+n = int(sys.stdin.readline().strip())
+events = defaultdict(int)
+
+for _ in range(n):
+    l, r = map(int, sys.stdin.readline().split())
+    events[l] += 1
+    events[r + 1] -= 1
+
+keys = sorted(events.keys())
+coverage = 0
+prev = keys[0]
+result = defaultdict(int)
+
+
+for point in keys:
+    result[coverage] += point - prev
+    coverage += events[point]
+    prev = point
+
+
+ans = [result[k] for k in range(1, n + 1)]
+print(*ans)
