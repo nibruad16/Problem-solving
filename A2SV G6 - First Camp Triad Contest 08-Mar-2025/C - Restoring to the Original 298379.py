3a21adae775063ea860366614bb4febdb7993e9d
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 02:39:42 +0300

    Created 'C - Restoring to the Original' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 08-Mar-2025/C - Restoring to the Original 298379.py
@@ -0,0 +1,7 @@
+# Problem: C - Restoring to the Original - https://codeforces.com/gym/589822/problem/C
+
+n = int(input())  
+s = input()  
+ones = s.count('n')  
+zeros = s.count('z')  
+print('1 ' * ones + '0 ' * zeros)
