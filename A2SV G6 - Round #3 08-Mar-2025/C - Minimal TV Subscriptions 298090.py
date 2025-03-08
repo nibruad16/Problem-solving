a8f5d7cc1be1766e588484e6354d8909aad35759
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-08 20:35:29 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 08-Mar-2025/C - Minimal TV Subscriptions 298090.py
@@ -0,0 +1,21 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+from collections import Counter
+t = int(input()) 
+for _ in range(t):
+    n, k, d = map(int, input().split())  
+    arr = list(map(int, input().split()))  
+    window = Counter(arr[:d]) 
+    ans = len(window) 
+    
+    for i in range(d, n):  
+        window[arr[i]] += 1 
+        window[arr[i - d]] -= 1  
+        
+        if window[arr[i - d]] == 0:  
+            del window[arr[i - d]]
+        
+        ans = min(ans, len(window)) 
+    
+    print(ans)  
+
