f0090b7de6d38411aafdda8205e00b95496bb936
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-08 20:00:40 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 08-Mar-2025/F - Array Transformation 298067.py
@@ -0,0 +1,21 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    count = Counter(arr)
+
+    values = list(count.values())
+    values.sort()
+
+    min_deletions = float('inf')
+    
+    for i in range(len(values)):
+
+        deletions = n - (len(values) - i) * values[i]
+        min_deletions = min(min_deletions, deletions)
+    
+    print(min_deletions)
