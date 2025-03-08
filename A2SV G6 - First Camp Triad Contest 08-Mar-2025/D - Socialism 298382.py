bacbdf45237c2183508cbbe32c06e644035abc48
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 02:49:17 +0300

    Created 'D - Socialism' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 08-Mar-2025/D - Socialism 298382.py
@@ -0,0 +1,15 @@
+# Problem: D - Socialism - https://codeforces.com/gym/589822/problem/D
+
+t = int(input())  
+
+for _ in range(t):  
+    n, x = map(int, input().split()) 
+    nums = sorted(map(int, input().split()), reverse=True) 
+    total = 0 
+    for i in range(n):  
+        total += nums[i]  
+        if total < x * (i + 1):  
+            print(i)  
+            break  
+    else:  
+        print(n) 
