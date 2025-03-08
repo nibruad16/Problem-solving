0e144bff45988a866f5e6b9324e4ae3cd982a479
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 02:25:43 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 08-Mar-2025/D - Minimize the Distance 298377.py
@@ -0,0 +1,8 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+points = [int(i) for i in input().split()]
+points.sort()
+
+mid_index = (n - 1) // 2
+print(points[mid_index])
