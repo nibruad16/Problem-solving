b989688731e015330f3f6eef7ce77fd7fb50773b
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 03:40:03 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/D - Life Across the Stars 298400.py
@@ -0,0 +1,25 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import defaultdict
+
+n = int(input())
+events = defaultdict(int)
+
+for _ in range(n):
+    birth, death = map(int, input().split())
+    events[birth] += 1
+    events[death] -= 1  
+
+years = sorted(events.keys())
+
+max_people = 0
+year = 0
+current_population = 0
+
+for y in years:
+    current_population += events[y]
+    if current_population > max_people:
+        max_people = current_population
+        year = y 
+
+print(year, max_people)
