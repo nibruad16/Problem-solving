3d2b00996e4b52a471410afb5d5ba9aa596ad6f9
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 03:47:16 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/B - Fix the Forecast! 298403.py
@@ -0,0 +1,19 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t = int(input().strip())  
+
+for _ in range(t):
+    n, k = map(int, input().split())  
+    a = list(map(int, input().split()))  
+    b = list(map(int, input().split()))  
+
+  
+    indexed_a = sorted(enumerate(a), key=lambda x: x[1])  
+    sorted_b = sorted(b)  
+
+    answer = [0] * n  
+    for i in range(n):
+        index = indexed_a[i][0] 
+        answer[index] = sorted_b[i]  
+
+    print(" ".join(map(str, answer)))  
