30175d02313e420b2fd3ca3ceb5fcd35f1f03a22
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-09 03:37:33 +0300

    Created 'A - Language Identifier' by A2SV Hub

---
+++ A2SV G6 - Round #5 09-Mar-2025/A - Language Identifier 298399.py
@@ -0,0 +1,12 @@
+# Problem: A - Language Identifier - https://codeforces.com/gym/591928/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    string = input()
+    if string[-1] == "u":
+        print("JAPANESE")
+    elif string[-1] == "o":
+        print("FILIPINO")
+    else:
+        print("KOREAN")
