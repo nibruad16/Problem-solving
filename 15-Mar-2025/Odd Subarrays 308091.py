b66fe7057cad3bbc682837d88da4bd0c3c3dbe66
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-15 23:54:57 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 15-Mar-2025/Odd Subarrays 308091.py
@@ -0,0 +1,20 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    dec_lengths = []
+    odd = 1
+
+    for i in range(1, n):
+        if nums[i] < nums[i - 1]:
+            odd += 1
+        else:
+            dec_lengths.append(odd)
+            odd = 1
+
+    dec_lengths.append(odd)
+    print(sum(length // 2 for length in dec_lengths))
