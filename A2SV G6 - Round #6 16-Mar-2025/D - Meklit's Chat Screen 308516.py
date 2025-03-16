267e320af4d971a1798eb2fa02fab42bbb26b1ee
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-16 21:47:07 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 16-Mar-2025/D - Meklit's Chat Screen 308516.py
@@ -0,0 +1,22 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+ans = deque()
+seen = set()
+
+for num in nums:
+    if num in seen:
+        continue 
+    
+    if len(ans) == k:
+        temp = ans.pop()
+        seen.remove(temp)
+
+    ans.appendleft(num)
+    seen.add(num)
+
+print(len(ans))
+print(*ans)
