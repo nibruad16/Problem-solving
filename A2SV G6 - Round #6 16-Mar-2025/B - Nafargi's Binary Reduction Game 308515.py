0ce634ccc2a6f4d59349130c0b1c3f038c516223
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-16 21:47:01 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 16-Mar-2025/B - Nafargi's Binary Reduction Game 308515.py
@@ -0,0 +1,9 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+n = int(input())
+s = input()
+
+count = s.count('0')
+temp = count * 2
+result = abs(n-temp)
+print(result)
