4fe09453d6629011e2907843f236187f66790598
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-03-16 21:47:18 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 16-Mar-2025/C - Permutation Minimization 308517.py
@@ -0,0 +1,26 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+from collections import deque
+x= int(input())
+for _ in range(x):
+    y = int(input())
+    nums = [int(i) for i in input().split()]
+
+    ans = deque()
+    ans.append(nums[0])
+    for i in range(1,len(nums)):
+        
+        right = ans[-1]
+        left = ans[0]
+        temp = nums[i]
+
+        if temp < right and temp > left:
+            ans.append(nums[i])
+        elif temp >= right:
+            ans.append(nums[i])
+        else:
+            ans.appendleft(nums[i])
+
+    print(*ans)
+        
+        
