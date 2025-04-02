2ac8e64561f603f7da2a729e56826aedaac161ef
nibruad <133175262+nibruad16@users.noreply.github.com>
2025-04-02 23:13:30 +0300

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 02-Apr-2025/A - Free Ice Cream for the Kids 324331.py
@@ -0,0 +1,25 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+n,x = map(int, input().split())
+temp = []
+for _ in range(n):
+    s = input()
+    temp.append(s)
+count = 0
+me = x
+for i in range(len(temp)):
+    now = temp[i].split()
+    
+    first = now[0]
+    last = int(now[-1])
+    if first == "+":
+         me += last
+    
+    if first == "-":
+        if me - last < 0:
+            count += 1
+        else:
+            me -= last     
+print(me,count)
+  
+
